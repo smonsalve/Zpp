a44eb3c50256b82eccb11b184d05ce49221f1380
alejandro <asalgad2@eafit.edu.co>
2015-03-05 14:29:06 -0500

    Class video ready

--- RTSP/MyImplementation/Video.java
+++ RTSP/MyImplementation/Video.java
@@ -1,5 +1,31 @@
-public class Video{
-    Video(){
+import java.io.*;
 
+public class Video{
+    
+    public FileInputStream file;
+    public int frame;    
+
+    Video(String fileName){
+        try{
+            file = new FileInputStream(fileName);
+            frame = 0;
+        }
+        catch(Exception a){
+            System.out.println(a.getMessage());
+        }
+    }
+
+    public int getNextFrame(byte[] frame) throws Exception{
+        int length;
+        String stringLength;
+
+        byte[] frameLength = new byte[5];
+
+        file.read(frameLength,0,5);
+
+        stringLength = new String(frameLength);
+        length = Integer.parseInt(stringLength);
+
+        return file.read(frame,0,length);
     }
 }
