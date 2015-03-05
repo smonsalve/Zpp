c8982259fdc5c87d47459c47ec3ea888fee3b1c9
alejandro <asalgad2@eafit.edu.co>
2015-03-05 13:35:35 -0500

    implementing client class

--- RTSP/MyImplementation/Client.java
+++ RTSP/MyImplementation/Client.java
@@ -1,6 +1,52 @@
-public class Client{
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
+
+public class Client implements ActionListener{
+
+    private Socket Csocket;
+
+    private static BufferedReader reader;
+    private static BufferedWriter writer;
+
+    private static String videoName;
+
+    private int secuence = 0;
+    private int id = 0;
+    private int MJPEG = 26;
+    
+    private static int INIT = 0;
+    private static int READY = 1;
+    private static int PLAYING = 2;
+    private static int state; 
+
+    private byte[] buffer;
 
     Client(){
+        buffer = new byte[15000];
+    }
 
+    public void actionPerformed(ActionEvent e){
+        String val = e.getActionCommand();
+        if(val.equals("SETUP")){
+            System.out.println("SETUP");                    
+        } 
+        else if(val.equals("PLAY")){
+            System.out.println("PLAY");                    
+
+        }
+        else if(val.equals("PAUSE")){
+            System.out.println("PAUSE");                    
+        
+        }
+        else if(val.equals("TEAR")){
+            System.out.println("TEAR");                    
+
+        }
+    }
+
+    public static void main(String[] argv){
+        Client client = new Client();
+        Clientframe frame = new Clientframe(client);
     }
 }
--- RTSP/MyImplementation/Clientframe.java
+++ RTSP/MyImplementation/Clientframe.java
@@ -1,6 +1,74 @@
-public class Clientframe extends JFrame{
-    
-    ClientFrame{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class Clientframe extends JFrame{
+        
+    //private Timer timer;
+
+    private JButton setUp;
+    private JButton play;
+    private JButton pause;
+    private JButton tear;
+
+    private JPanel main;
+    private JPanel button;
+
+    private JLabel label;
+    private ImageIcon icon;
+
+    Clientframe(Client client){
+        super("Client");
+
+        //timer = new Timer(20, new TimerListener());
+        //timer.setInicialDelay(0);
+        //timer.setCoalesce(true);
+
+        setUp = new JButton("Setup");
+        play = new JButton("Play");
+        pause = new JButton("pause");  
+        tear = new JButton("tear");
+
+        main = new JPanel();
+        button = new JPanel();
+        label = new JLabel();
+
+        addWindowListener(                                            
+            new WindowAdapter(){
+                public void windowClosing(WindowEvent e){
+           //         timer.stop();
+                    System.exit(0);
+                }
+            }
+        );     
+
+        label.setIcon(null);
+        
+        button.setLayout(new GridLayout(1,0));
+        button.add(setUp);
+        button.add(play);
+        button.add(pause);
+        button.add(tear);
+        
+        setUp.addActionListener(client);
+        play.addActionListener(client);
+        pause.addActionListener(client);
+        tear.addActionListener(client);
+
+        setUp.setActionCommand("SETUP");
+        play.setActionCommand("PLAY");
+        pause.setActionCommand("PAUSE");
+        tear.setActionCommand("TEAR");
+        
+        main.setLayout(null);
+        main.add(label);
+        main.add(button);
+        
+        label.setBounds(0,0,380,280);
+        button.setBounds(0,280,390,50);
+
+        getContentPane().add(main, BorderLayout.CENTER);
+        setSize(new Dimension(390, 370));
+        setVisible(true);
     }
 }
