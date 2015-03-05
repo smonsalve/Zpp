631b5873ce00b81917980702d5b30de93d5f4357
alejandro <asalgad2@eafit.edu.co>
2015-03-05 14:18:28 -0500

    implementing the package

--- RTSP/MyImplementation/Packet.java
+++ RTSP/MyImplementation/Packet.java
@@ -1,5 +1,39 @@
 public class Packet{
-    Packet(){
 
+    private int headerSize = 12;
+    private int playloadSize;
+
+    public int version;
+    public int padding;
+    public int extension;
+    public int cc;
+    public int marker;
+    public int payloadType;
+    public int sequenceNumber;
+    public int timeStamp;
+    public int ssrc;
+
+    public byte[] header;
+    public byte[] payload;
+    
+ 
+    Packet(int playloadType, int secuenceNumber, int timeStamp, byte[] data, int payloadSize){
+    
+        version = 2;
+        padding = 0;
+        extension = 0;
+        cc = 0;
+        marker = 0;
+        ssrc = 0;
+
+        this.secuenceNumber = secuenceNumber; 
+        this.timeStamp = timeStamp;
+        this.payloadType = payloadType; 
+
+        header = new byte[headerSize];
+        //fill the header with the data
+
+        this.payloadSize = payloadSize;
+        payload = new byte[payloadSize];
     }
 }
